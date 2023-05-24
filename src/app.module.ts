80d70f931aad94c6560f979a8dec681fd85d23bd
Kelvin <kelvincheah02@gmail.com>
2023-05-24 18:46:29 +0800

    typegraphql

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { AuthorModule } from './author/author.module';
 import { BookModule } from './book/book.module';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
@@ -12,7 +13,16 @@ import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
       driver: ApolloDriver,
       autoSchemaFile: 'schema.gql',
     }),
-
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'kelvin-database.c37aly7ftlww.ap-southeast-1.rds.amazonaws.com',
+      port: 3000,
+      username: 'kelvinc',
+      password: 'EdXbQ-VygQrBzElx8bHkQ5G_W2cm8Xyy',
+      database: 'kelvin_database',
+      entities: [__dirname + '/../**/*.entity.js'],
+      synchronize: true,
+    }),
     AuthorModule,
     BookModule,
   ],
